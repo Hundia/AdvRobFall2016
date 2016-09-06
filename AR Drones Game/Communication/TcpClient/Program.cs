b033c93be0541680f4ac707edaecca18d0e1e3ac
eli hundia <eli.hundia@gmail.com>
2016-09-06 08:38:57 +0300

    Drone Game Comm

---
+++ AR Drones Game/Communication/TcpClient/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace UnityTcpClient
+{
+
+    //  A Cool tcp client for the Battleship project :-)
+    //  By Eli Hundia
+    class Program
+    {
+        private string ipString;
+        private int connectionPort;
+        TcpClient tcpclnt;
+        NetworkStream stm;
+        ASCIIEncoding asen;
+        public void init(string ip, int port)
+        {
+            ipString = ip;
+            connectionPort = port;
+            asen = new ASCIIEncoding();
+        }
+
+        public void connect()
+        {
+            //  Create a TCP Client
+            tcpclnt = new TcpClient();
+            try
+            {
+                //  Connect to the server
+                Console.WriteLine("Connecting.....");
+                tcpclnt.Connect(ipString, connectionPort);
+                Console.WriteLine("Connected");
+                stm = tcpclnt.GetStream();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error connecting to server: " + e.StackTrace);
+            }
+        }
+
+        public void sendMsg(string msgToSend)
+        {
+            try
+            {
+                byte[] ba = asen.GetBytes(msgToSend);
+                stm.Write(ba, 0, ba.Length);
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Error Writing to server: " + e.StackTrace);
+            }
+        }
+
+        public void closeConnection()
+        {
+            try {
+                tcpclnt.Close();
+            }
+            
+            catch (Exception e)
+            {
+                Console.WriteLine("Error Closing the socket: " + e.StackTrace);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Program client = new Program();
+            client.init("127.0.0.1", 8001);
+            client.connect();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("Enter the string to be transmitted : ");
+                String str = Console.ReadLine();
+                client.sendMsg(str);
+            }
+
+            client.closeConnection();
+            
+        }
+    }
+}
---
+++ AR Drones Game/Communication/TcpServer/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TcpServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                IPAddress ipAd = IPAddress.Parse("127.0.0.1");
+                // use local m/c IP address, and 
+                // use the same in the client
+
+                /* Initializes the Listener */
+                TcpListener myList = new TcpListener(ipAd, 8001);
+
+                /* Start Listeneting at the specified port */
+                myList.Start();
+
+                Console.WriteLine("The server is running at port 8001...");
+                Console.WriteLine("The local End point is  :" +
+                                  myList.LocalEndpoint);
+                Console.WriteLine("Waiting for a connection.....");
+
+                Socket s = myList.AcceptSocket();
+                Console.WriteLine("Connection accepted from " + s.RemoteEndPoint);
+
+                while (true)
+                {
+                    byte[] b = new byte[100];
+                    int k = s.Receive(b);
+                    if (k == 0)
+                    {
+                        break;
+                    }
+                    Console.WriteLine("Recieved...");
+                    for (int i = 0; i < k; i++)
+                        Console.Write(Convert.ToChar(b[i]));
+
+                  //  ASCIIEncoding asen = new ASCIIEncoding();
+                   // s.Send(asen.GetBytes("The string was recieved by the server."));
+                   // Console.WriteLine("\nSent Acknowledgement");
+                }
+                
+                /* clean up */
+                s.Close();
+                myList.Stop();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error..... " + e.StackTrace);
+            }   
+        }
+    }
+}
